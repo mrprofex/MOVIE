f39ab0c4a5385362adffa799c6758379edf48023
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-06 13:09:35 +0530

    set up server in the inde.js file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const PORT 
 const app = express();
-app.listen()
+app.listen(PORT , ()=>{
+    console.log("Your server is running on:http://localhost:3000")
+})
