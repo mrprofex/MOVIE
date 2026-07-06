60f1ff76a2690c582c35e78e91c419d06ad8ce0a
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-06 13:40:11 +0530

    creating signup route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const api = require("./config/prisma")
 require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000 
 const app = express();
 //mid
 app.use(express.json())
@@ -10,7 +11,17 @@ app.use(express.urlencoded({extended : false }))
 // sign up  route
 app.post("/signup", async()=>{
     try {
-        const {} = req.body;
+        const {name , email , password} = req.body;
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message : "Please provide all the details Name , email , Password"
+            })
+        }
+        const userExist = await api.user.findUnique({
+            where : {
+                email ,
+            }
+        })
     } catch (error) {
         console.error(error);
         return res.status(500).json({
