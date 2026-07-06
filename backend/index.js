b0e3a5b2414355f8fb6bc218fb417b7f8f314e3b
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-06 13:23:49 +0530

    added config and prisma.js file

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,23 @@ const app = express();
 //mid
 app.use(express.json())
 app.use(express.urlencoded({extended : false }))
+
+// sign up  route
+app.post("/signup", async()=>{
+    try {
+        const 
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message : "Server Error"
+        })
+    }
+})
+
+// sign in route 
+
+
+
 app.listen(PORT , ()=>{
     console.log("Your server is running on:http://localhost:${PORT}")
 })
