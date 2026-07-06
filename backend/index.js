1dbdb4aa9fd42566bbd97a2f69e5a4ebd8f88aa7
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-06 13:14:34 +0530

    added some middle ware

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 require("dotenv").config();
+
 const PORT = process.env.PORT
 const app = express();
+//mid
+app.use(express.json())
+app.use(express.urlencoded({extended : false }))
 app.listen(PORT , ()=>{
     console.log("Your server is running on:http://localhost:${PORT}")
 })
