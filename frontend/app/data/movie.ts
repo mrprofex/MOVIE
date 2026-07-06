ac9328bbd0e3249f4026474987597c1b210a9450
Akshay Kumar <akshaykumargts2905@gmail.com>
2026-07-06 14:40:21 +0530

    movie

---
+++ frontend/app/data/movie.ts
@@ -0,0 +1,60 @@
+const movie = [
+  {
+    id: 1,
+    title: "KGF",
+    genre: "Action",
+    year: 2018,
+    poster: " posters/KGF.jpg",
+  },
+  {
+    id: 2,
+    title: "RRR",
+    genre: "Action",
+    year: 2022,
+    poster: " posters/RRR.jpg",
+  },
+  {
+    id: 3,
+    title: "Pushpa",
+    genre: "Action",
+    year: 2021,
+    poster: " posters/Pushpa.jpg",
+  },
+  {
+    id: 4,
+    title: "Bahubali",
+    genre: "Fantasy",
+    year: 2015,
+    poster: "posters/bahubali.jpg",
+  },
+  {
+    id: 5,
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    year: 2014,
+    poster: "posters/Interstellars.jpg",
+  },
+  {
+    id: 6,
+    title: "Salaar",
+    genre: "Action",
+    year: 2023,
+    poster: "posters/Salar.jpg",
+  },
+  {
+    id: 7,
+    title: "Leo",
+    genre: "Action",
+    year: 2023,
+    poster: "posters/leo.jpg",
+  },
+  {
+    id: 8,
+    title: "Avengers Endgame",
+    genre: "Action",
+    year: 2019,
+    poster: "posters/Endgame.jpg",
+  },
+];
+
+export default movie;
