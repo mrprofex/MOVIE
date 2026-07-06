bd0177c277cecb6f4771e737a7dcfaa3609a414f
mrprofex <vivekkumarpatar2007@gmail.com>
2026-07-06 14:26:44 +0530

    signup route is working properly

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : false }))
 
 // sign up  route
-app.post("/signup", async()=>{
+app.post("/signup", async(req , res)=>{
     try {
         const {name , email , password} = req.body;
         if(!name || !email || !password){
